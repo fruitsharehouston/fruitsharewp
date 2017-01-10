27487de121d8234704cca66578dc4f8e7c8d4bc2
Nha Huynh <nhahuynhwrites@gmail.com>
2017-01-10 00:13:42 -0500

    add template files

---
+++ footer.php
@@ -0,0 +1,27 @@
+ <footer>
+    <div class="container-fluid">
+     
+      <a class="social">
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+      </a>
+      <a class="social">
+        <i class="fa fa-facebook" aria-hidden="true"></i>
+      </a>
+      <a class="social">
+        <i class="fa fa-instagram" aria-hidden="true"></i>
+      </a>
+      
+      <ul class="contact-info">
+        <li>713-000-0000</li>
+        <li>-  500 Fruit Circle, Houston, TX</li>      
+      </<ul>
+      
+    </div>
+  </footer>
+
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="./scripts/main.js"></script>
+<?php wp_footer(); ?> 
+</body>
+</html>
