1a55f4edc5ed704272ab5e09b3bfb7a9d1066f8f
nhahuynh <nhahuynhwrites@gmail.com>
2017-02-09 19:00:07 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,10 +11,9 @@
         <i class="fa fa-instagram" aria-hidden="true"></i>
       </a>
 
-      <ul class="contact-info">
-        <li>713-000-0000</li>
-        <li>-  500 Fruitshare Circle, Houston, TX</li>
-      </<ul>
+      <p class="contact-info">
+       Email us at <a href="fruitsharehouston@gmail.com">fruitsharehouston@gmail.com</a>
+      </p>
 
     </div>
   </footer>
