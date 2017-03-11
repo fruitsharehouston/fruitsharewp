6a9a36ce77b711d4ff4ced92cb15def266a5300c
nhahuynh <nhahuynhwrites@gmail.com>
2017-03-11 17:38:27 -0600

    yet another slideshow

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
               <h1>100,000 pounds <strong>picked</strong> &amp; <strong>shared</strong> last season!</h1>
             </div>
           </div>-->
-         <?php if (function_exists('slideshow')) { slideshow($output = true, $gallery_id = false, $post_id = false, $params = array()); } ?>
+         if ( function_exists( 'soliloquy' ) ) { soliloquy( '90' ); }
         </div>
       </div>
 
