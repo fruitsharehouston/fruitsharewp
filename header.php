7b3a2b4b3f22206e2a0e1c501bb05b1f579ccc87
nhahuynh <nhahuynhwrites@gmail.com>
2017-05-14 16:12:25 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <link id="favicon" rel="icon" href="//fruitsharehouston.github.io/images/favicon.ico" type="image/x-icon">
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" >
 
   <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
